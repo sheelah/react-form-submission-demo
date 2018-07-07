0d297fb0166a58046fc0ff04e0c29e6c47b3072d
Sheelah Brennan <slb53@hotmail.com>
2018-07-07 14:44:59 -0600

    Add .env
    This allows us to make any environment variables
    available to the entire application for usage in
    components and tests.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { env } from './config';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App env={env} />, document.getElementById('root'));
+
 registerServiceWorker();
