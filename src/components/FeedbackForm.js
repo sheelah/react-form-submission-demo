7a99a0a3df2fcade9b717c040549673cb79a9560
Sheelah Brennan <slb53@hotmail.com>
2020-11-24 11:43:09 -0700

    Migrate FeedbackForm to functional components & hooks

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,96 +1,98 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-export default class FeedbackForm extends Component {
-  state = {
-    feedback: '',
-    formSubmitted: false
+const FeedbackForm = ({ env }) => {
+  const [feedback, setFeedback] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [formSubmitSuccessful, setFormSubmitSuccessful] = useState(false);
+
+  const sender = 'sender@example.com';
+
+  const handleCancel = () => {
+    setFeedback('');
   };
 
-  handleCancel = this.handleCancel.bind(this);
-  handleChange = this.handleChange.bind(this);
-  handleSubmit = this.handleSubmit.bind(this);
+  const handleChange = (event) => {
+    setFeedback(event.target.value);
+  };
 
-  static sender = 'sender@example.com';
-
-  handleCancel() {
-    this.setState({
-      feedback: ''
-    });
-  }
-
-  handleChange(event) {
-    this.setState({
-      feedback: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const {
       REACT_APP_EMAILJS_RECEIVER: receiverEmail,
       REACT_APP_EMAILJS_TEMPLATEID: template,
       REACT_APP_EMAILJS_USERID: user,
-    } = this.props.env;
+    } = env;
 
-    this.sendFeedback(
-      template,
-      this.sender,
+    sendFeedback({
+      templateId: template,
+      senderEmail: sender,
       receiverEmail,
-      this.state.feedback,
-      user
-    );
-
-    this.setState({
-      formSubmitted: true
+      feedback,
+      user,
     });
-  }
 
- // Note: this is using default_service, which will map to whatever
- // default email provider you've set in your EmailJS account.
-  sendFeedback(templateId, senderEmail, receiverEmail, feedback, user) {
+    setFormSubmitted(true);
+  };
+
+  // Note: this is using default_service, which will map to whatever
+  // default email provider you've set in your EmailJS account.
+  const sendFeedback = ({
+    templateId,
+    senderEmail,
+    receiverEmail,
+    feedback,
+    user,
+  }) => {
     window.emailjs
-      .send('default_service', templateId, {
+      .send(
+        'default_service',
+        templateId,
+        {
           senderEmail,
           receiverEmail,
-          feedback
+          feedback,
         },
         user
       )
-      .then(res => {
-        this.setState({
-          formEmailSent: true
-        });
+      .then((res) => {
+        if (res.status === 200) {
+          setFormSubmitSuccessful(true);
+        }
       })
       // Handle errors here however you like
-      .catch(err => console.error('Failed to send feedback. Error: ', err));
+      .catch((err) => console.error('Failed to send feedback. Error: ', err));
+  };
+
+  if (formSubmitted && formSubmitSuccessful) {
+    return <h2>Thank You! Your submission was sent.</h2>;
   }
 
-  render() {
-    return (
-      <form className="feedback-form" onSubmit={this.handleSubmit}>
-        <h1>Your Feedback</h1>
-        <textarea
-          className="text-input"
-          id="feedback-entry"
-          name="feedback-entry"
-          onChange={this.handleChange}
-          placeholder="Enter your feedback here"
-          required
-          value={this.state.feedback}
-        />
-        <div className="btn-group">
-          <button className="btn btn--cancel" onClick={this.handleCancel}>
-            Cancel
-          </button>
-          <input type="submit" value="Submit" className="btn btn--submit" />
-        </div>
-      </form>
-    );
-  }
-}
+  return (
+    <form className='feedback-form' onSubmit={handleSubmit}>
+      <h1>Your Feedback</h1>
+      <textarea
+        className='text-input'
+        id='feedback-entry'
+        name='feedback-entry'
+        onChange={handleChange}
+        placeholder='Enter your feedback here'
+        required
+        value={feedback}
+      />
+      <div className='btn-group'>
+        <button className='btn btn--cancel' onClick={handleCancel}>
+          Cancel
+        </button>
+        <input type='submit' value='Submit' className='btn btn--submit' />
+      </div>
+    </form>
+  );
+};
 
 FeedbackForm.propTypes = {
-  env: PropTypes.object.isRequired
+  env: PropTypes.object.isRequired,
 };
+
+export default FeedbackForm;
